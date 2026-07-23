4398fcfd143781101b6e31a49b2d88b4ee28b21c
Styx <styx@styxnotes.com>
2026-07-23 16:54:25 +0300

    Create nix package

---
+++ default.nix
@@ -0,0 +1,30 @@
+{ stdenv, lib, fetchFromGitHub, nushell }:
+
+stdenv.mkDerivation rec {
+  pname = "alter";
+  version = "0.0.1";
+
+  src = ./.;
+
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/{bin,share/${pname}}
+    cp -r * $out/share/${pname}
+    bin=$out/bin/${pname}
+    cat > $bin <<EOF
+      #!/bin/sh -e
+      exec $out/share/${pname}/${pname} "\$@"
+    EOF
+    chmod +x $bin
+  '';
+
+  propagatedBuildInputs = [ nushell ];
+
+  meta = with lib; {
+    description = "A cli interface to Alter tracker.";
+    homepage = "https://github.com/kaizen4dev/alter-cli";
+    license = licenses.mit;
+    platforms = platforms.unix;
+  };
+}
